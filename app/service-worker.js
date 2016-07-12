332a4b194684cc35182bc10ecf1092f0dde95ab8
Mariusz Dobrowolski <Mariusz Dobrowolski>
2016-07-12 22:30:54 +0200

    add json cache

--- app/service-worker.js
+++ app/service-worker.js
@@ -24,9 +24,76 @@ var REQUEST_TO_CACHE = [
 ];
 
 var heartbeatUrl = 'http://localhost:3000/heartbeat';
+var onlineMode = true;
+
+function serialize(request) {
+  var headers = {};
+  for (var entry of request.headers.entries()) {
+    headers[entry[0]] = entry[1];
+  }
+  var serialized = {
+    url: request.url,
+    headers: headers,
+    method: request.method,
+    mode: request.mode,
+    credentials: request.credentials,
+    cache: request.cache,
+    redirect: request.redirect,
+    referrer: request.referrer
+  };
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    return request.clone().text().then(function(body) {
+      serialized.body = body;
+      return Promise.resolve(serialized);
+    });
+  }
+  return Promise.resolve(serialized);
+}
+
+function deserialize(data) {
+  return Promise.resolve(new Request(data.url, data));
+}
+
+function sendInOrder(requests) {
+    var sending = requests.reduce(function(prevPromise, serialized) {
+    console.log('Sending', serialized.method, serialized.url);
+    return prevPromise.then(function() {
+      return deserialize(serialized).then(function(request) {
+        return fetch(request);
+      });
+    });
+  }, Promise.resolve());
+  return sending;
+}
+
+function flushQueue() {
+    return localforage.getItem('queue').then(function(queue) {
+    queue = queue || [];
+        if (!queue.length) {
+      return Promise.resolve();
+    }
+    console.log('Sending ', queue.length, ' requests...');
+    return sendInOrder(queue).then(function() {
+        return localforage.setItem('queue', []);
+    });
+  });
+}
+
+function enqueue(request) {
+  return serialize(request).then(function(serialized) {
+    localforage.getItem('queue').then(function(queue) {
+      queue = queue || [];
+      queue.push(serialized);
+      return localforage.setItem('queue', queue).then(function() {
+        console.log(serialized.method, serialized.url, 'enqueued!');
+      });
+    });
+  });
+}
 
 function synchronizeContent(){
     console.log('synchronizing offline content');
+    flushQueue();
 }
 
 function checkServerHeartbeat(){
@@ -42,8 +109,11 @@ function checkServerHeartbeat(){
 
     fetch(myRequest, myInit).then(function(response){
         if(response.status == 200){
+            onlineMode = true;
             synchronizeContent();
         }
+    }).catch(function(err){
+        onlineMode = false;
     });
 }
 
@@ -69,38 +139,36 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-          if (response) {
-              console.log(
-                '[fetch] Returning from ServiceWorker cache: ',
-                event.request.url
-              );
-
-              if (RESPONSE_TO_CACHE.indexOf(event.request.url) > -1){
-                  fetch(event.request).then(function(response){
-                    return caches.open(CACHE_NAME).then(function(cache) {
-                    cache.put(event.request.url, response.clone());
-                    console.log('[fetch] Cache json result for url: ' + event.request.url);
-                    return response;
-                  })}).catch(function(err){
-                     return response;
-                  });
+      .then(function(cache) {
+          if(RESPONSE_TO_CACHE.indexOf(event.request.url) > -1){
+              if(onlineMode){
+                  return fetch(event.request).then(function(response){
+                    return caches.open(CACHE_NAME).then(function(store) {
+                        store.put(event.request.url, response.clone());
+                        console.log('[fetch] Cache json result for url: ' + event.request.url);
+                        return response;
+                   })}).catch(function(err){
+                         onlineMode = false;
+                         return cache || fetch(event.request);
+              });
+              } else {
+                  return cache || fetch(event.request);
               }
-
-              return response;
-          } else if(RESPONSE_TO_CACHE.indexOf(event.request.url) > -1){
-              fetch(event.request).then(function(response){
-                    return caches.open(CACHE_NAME).then(function(cache) {
-                    cache.put(event.request.url, response.clone());
-                    console.log('[fetch] Cache json result for url: ' + event.request.url);
-                    return response;
-                  })}).catch(function(err){
-                     response;
-                  });
+          } else if (cache) {
+              console.log('[fetch] Returning from ServiceWorker cache: ', event.request.url);
+              return cache;
+          } else if(REQUEST_TO_CACHE.indexOf(event.request.url) > -1){
+              if(onlineMode){
+                  return fetch(event.request);
+              } else {
+                  return enqueue(event.request).then(function(){
+                      return new Response({status: 200}).clone();
+                  })
+              }
+          }  else {
+              console.log('[fetch] Returning from server: ', event.request.url);
+              return fetch(event.request);
           }
-
-          console.log('[fetch] Returning from server: ', event.request.url);
-          return fetch(event.request);
       }
     )
   );
